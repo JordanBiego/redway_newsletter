25fa378ce1a959d236986e0601787cd40d6d09e0
JordanBiego <jordanbiego@gmail.com>
2024-02-23 14:55:46 -0300

    fix: :bug: Fix sendgrid api key mistake

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,7 +2,7 @@ class LeadsController < ApplicationController
   def create
     @lead = Lead.create(name: params[:name], email: params[:email])
     if @lead.save
-      LeadMailer.welcome_email(@lead, I18n.locale).deliver_now
+      # LeadMailer.welcome_email(@lead, I18n.locale).deliver_now
       flash[:success] = t('leads.create.success')
     else
       flash[:alert] = t('leads.create.alert')
