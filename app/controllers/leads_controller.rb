a65f8a2a5ab82241d3ed5ac5c4b13031fbb8b19e
JordanBiego <jordanbiego@gmail.com>
2024-02-23 15:31:22 -0300

    fix: :bug: Fix email delivery system

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,12 +1,18 @@
 class LeadsController < ApplicationController
   def create
     @lead = Lead.create(name: params[:name], email: params[:email])
+
     if @lead.save
-      # LeadMailer.welcome_email(@lead, I18n.locale).deliver_now
       flash[:success] = t('leads.create.success')
+      begin
+        LeadMailer.welcome_email(@lead, I18n.locale).deliver_now
+      rescue StandardError => e
+        Rails.logger.error("Error delivering email: #{e.message}")
+      end
     else
       flash[:alert] = t('leads.create.alert')
     end
+
     redirect_to :root
   end
 end
